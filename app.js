4ed1fd0eea276d81701f552981cd8aea47fec68d
Godsent Clem <gs.trinity@outlook.com>
2023-09-16 17:43:48 +0100

    initial https methods applied

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');  // here we import our express framework
 const app = express();
 
+const db = require('./models'); // new require for db object
+
 const port = 8080;
 
 app.use(express.json());
@@ -14,6 +16,15 @@ app.use(
 app.get('/', (req, res)=>{
     res.json({message: "it's working"})
 });
+
+app.get('/api/contacts', (req, res) => {
+    return db.Contact.findAll()
+      .then((contacts) => res.json(contacts))
+      .catch((err) => {
+        console.log('There was an error querying contacts', JSON.stringify(err))
+        return res.send(err)
+      });
+  });  
 
 // Start the server
 app.listen(port, () => {
